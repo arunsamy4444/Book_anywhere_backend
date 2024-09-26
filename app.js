c10ca4c582d982cc140635ada2716bc282e857f2
Arun Samy <134082910+arunsamy4444@users.noreply.github.com>
2024-09-26 18:11:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PlacesRouter = require('./router/place_router');
+const userRouter = require('./router/users_router');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use("/api", userRouter);
+app.use("/api", PlacesRouter);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to the database');
+})
+.catch((err) => {
+    console.error('Error connecting to the database', err);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
